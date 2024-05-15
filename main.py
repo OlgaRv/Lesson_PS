877528ada68f66064f28673c7cdb07e58c8066a7
OlgaRv <olgavr2959@gmail.com>
2024-05-15 06:24:09 +0300

    New ecxamples

--- main.py
+++ main.py
@@ -1,3 +1,51 @@
+# Напишите программу, с помощью которой можно искать информацию на Википедии с помощью консоли.
+# 1. Спрашивать у пользователя первоначальный запрос.
+# 2. Переходить по первоначальному запросу в Википедии.
+# 3. Предлагать пользователю три варианта действий:
+#    a) листать параграфы текущей статьи;
+#    b) перейти на одну из связанных страниц —
+#       и снова выбор из двух пунктов:
+#        - листать параграфы статьи;
+#        - перейти на одну из внутренних статей.
+#    c) выйти из программы.
+
+from selenium import webdriver
+from selenium.webdriver import Keys
+from selenium.webdriver.common.by import By
+import time
+import random
+
+# поиск контента
+
+browser = webdriver.Chrome()
+browser.get('https://ru.wikipedia.org/wiki/%D0%A1%D0%BE%D0%BB%D0%BD%D0%B5%D1%87%D0%BD%D0%B0%D1%8F_%D1%81%D0%B8%D1%81%D1%82%D0%B5%D0%BC%D0%B0')
+
+hatnotes = []
+
+for element in browser.find_elements(By.TAG_NAME, "div"):
+    cl = element.get_attribute("class")
+    if cl == "hatnote navigation-not-searchable":
+        hatnotes.append(element)
+
+# print(hatnotes)
+
+hatnote = random.choice(hatnotes)
+link = hatnote.find_element(By.TAG_NAME, "a").get_attribute("href")
+browser.get(link)
+time.sleep(10)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
